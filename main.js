b0517821e9ffd9dd0d2841c6d54b81c9f279b900
farhia-siyaad <148630711+farhia-siyaad@users.noreply.github.com>
2023-11-26 11:50:45 +0300

    this is light

---
+++ main.js
@@ -0,0 +1,656 @@
+
+// this displays text inthe console
+// console.log("Hello World");
+
+
+// this line displays text in the browse
+
+// document.write("Farhia Mohamed Siyaad")
+// document.write("Farhia Mohamed Siyaad")
+
+/*
+document.write("Farhia Mohamed Siyaad")
+document.write("Farhia Mohamed Siyaad")
+document.write("Farhia Mohamed Siyaad")
+
+*/
+// this variable
+// let age =60;
+// console.log(age);
+
+// age=1
+
+// document.write(age)
+
+// document.write("<br>")
+// const number=10
+// console.log(number)
+// document.write(number)
+
+
+// this is string type
+// let name="Kaafi"
+//this is integer
+// let age= 19;
+//this boolean
+// let ismale="true"
+//this is double
+// let price=10.2;
+
+// document.write(name  )
+// document.write("<br>")
+// document.write(age )
+// document.write("<br>")
+// document.write( ismale)
+// document.write("<br>")
+// document.write(price )
+
+// let num1= 10;
+// let num2= 3;
+// console.log(num1 + num2);
+// console.log(num2 - num1);
+// console.log(num2 * num1);
+// console.log(num1 / num2);
+// console.log(num1 **2);
+// console.log(num1 % num2);
+
+
+
+// let ages = 20;
+// // age++
+// // age++
+// ages +=20
+// ages -=10
+// console.log(ages);
+
+
+// let my_Name= "Farhia"
+// console.log("my Name is "+ my_Name);
+// document.write("My  Name is", my_Name )
+
+// console.log(`my Name is ${my_Name} `);
+
+// document.write(`my Name is  ${my_Name} `)`
+ 
+// let  name= "Abdi"
+// let age= 70;
+// document.write(`there was once a man called ${name} He was  ${age} years old <br> 
+// he liked his Name
+//   ${name}  but <br> he didn't  being  ${age} years old `)
+//  let num1=20
+//  let num2=20
+// document.write(`the addition of ${num1} and ${num2}  is `,num1+num2)
+
+// let name= "Ali";
+// if (name== "Ali") {
+//      document.write("you are Ali  <br>");
+     
+// }
+// let age=15;
+// if (age==1) {
+//      document.write("You Are Adult")
+     
+// }
+
+// let ages=15;
+// if (ages<16) {
+//      console.log("waxaad ka yartahay  16");
+     
+// }
+
+// let age=15;
+// if (age>=15) {
+//      console.log("waad ka weyntahay ama waad la egtahay");
+     
+// }
+
+
+
+
+
+// let username="admin";
+// let password="admin123"
+
+
+
+// if (username == "admin2" && 
+// password== "admin123") {
+//      console.log("Login Successfully");
+     
+// }
+
+
+// if (username=="admin" || password==1234) {
+//      console.log("success");
+// }
+
+
+
+
+// if (username == "admin1" && 
+// password== "admin123") {
+//      console.log("Login Successfully");
+     
+// }
+
+
+// else{
+// console.log("username or password is incorrect");
+// }
+
+
+
+// let age= 15;
+// if (age>=15) {
+//     console.log("you are adult"); 
+// }
+// else{
+//      console.log("You Are Under Age")
+// }
+
+
+//this Email with prompt function
+
+// // let email="farax123@gmail.com"
+// // let password= 123
+// // if (username == "admin1" && 
+// // password== "admin123") {
+// //      console.log("Login Successfully");
+     
+// }
+
+// let email= prompt("Enter Your Email")
+// let password= prompt("Enter Your passwordl")
+// if ( email == "farax@gmail.com" && 
+// password== "farax") {
+//      console.log("Successfully");
+     
+// }
+// else{
+//      console.log("Decline");
+// }
+
+// mark=80;
+
+// if (mark==100) {
+//      console.log("Grade A+");
+// }
+// else if(mark ==90){
+//      console.log("Grade B");
+// }
+// else if (mark>=80) {
+//      console.log("B+");
+// }
+// else{
+//      console.log("fail");
+// }
+
+
+// let name= prompt("Enter your Name")
+// if (name =="Ali") {
+//      console.log("Welcome ",name);
+// }
+// else if(name== "Abdi"){
+//      console.log("Welcome",name );
+// }
+// else if(name=="muna"){
+//      console.log("Welcome",name);
+// }
+// else{
+//      console.log("inta laguu ma ogolo Fadlan dib u laawo");
+// }
+
+
+// let grade= prompt("Enter Your marks")
+
+// if (grade >100) {
+//      console.log("unknown grade");
+// }
+// else if (grade>=90) {
+//      console.log("Grade A+");
+// }
+// else if (grade == 89 || grade >=79) {
+//      console.log("Grade B+");
+// }
+// else if (grade == 69 || grade >=59) {
+//      console.log("Grade C+");
+// }
+// else if (grade == 50) {
+//      console.log("Minimal pass");
+// }
+
+// else if (grade<50) {
+//      console.log("fail");
+// }
+
+
+
+// let byear= prompt("Enter Your Birth day Year ");
+// let cyear= prompt ("Enter current Year")
+// let age= cyear-byear;
+// console.log ( "You are ",age ,"Years old");
+
+
+
+
+
+// let grade= prompt("Enter Your marks")
+
+// if (grade >100) {
+//      console.log("unknown grade");
+// }
+// else if (grade>=90) {
+//      console.log("Grade A");
+// }
+// else if (grade >= 80|| grade >=89) {
+//      console.log("Grade B");
+// }
+// else if (grade >= 70 || grade >=79) {
+//      console.log("Grade C");
+// }
+// else if (grade >= 60 || grade >=69) {
+//      console.log("Grade D");
+// }
+
+
+// else if (grade<60) {
+//      console.log("fail");
+// }
+
+
+
+
+
+// let num=1;
+// while (num<=10) {
+//      console.log(num);
+//      num++
+// }
+
+
+
+// number=1
+// while (number<=50) {
+//      console.log(number);
+//      number++;
+// }
+
+
+
+
+
+
+
+
+//  let name=prompt("Enter your name")
+//  for(let i=0; i<=10; i++){
+// document.write(name ,"<br>");
+//  }
+
+
+
+//  let name=prompt("Enter your name")
+//  let num= 1;
+//  while(num <=10){
+//      console.log( "here",name);
+//      document.write(num, " ",name ,"<br>")
+//      num++;
+//  }
+//for loop 20 novenber 2023
+// for(let num=1; num<=5; num++){
+//     console.log(num);
+// }
+
+// for(let num=100; num>=1; num--){
+// console.log(num);
+// }
+
+
+// let name= prompt("Enter Your name")
+
+
+// for(let num =1; num<=15; num++){
+//     console.log(num);
+//     console.log(name);
+//     document.write( num,name , "<br>")
+// }
+
+
+// let number=1
+// do {
+//     console.log(number);
+//     number++
+// } while (number<=5);
+
+
+// let name= prompt("Enter your name")
+// let age = prompt("Enter Your age")
+// let num=1
+// do{
+//    document.write("Hello  ", name ," Your are ",age ," Years old <br>")
+//     num++
+// }while (num<=5) 
+
+
+
+// let num=5
+// do{
+//     document.write(num ," ")
+//     num=num+5
+// }while(num<=40)
+
+
+
+
+// function sayhi() {
+//     console.log("Hello My Name is Farhia");
+// document.write("Hello My Name is Farhia");
+    
+// }
+// sayhi()
+
+
+// function sum(num1,num2){
+//     console.log(num1+num2);
+//     document.write(num1+num2)
+// }
+//  sum(2,2)
+//this is simple Function
+//  function iskudar(){
+//     let num1=10
+//     let num2= 40
+//     let sum =num1+num2
+//     console.log(sum);
+//  } iskudar()
+//function that takes prameters and arguments
+// function myname(name) {
+//     console.log("Hello", name);
+// }
+// myname("Ali")
+
+
+
+// function names(name1, name2,name3,name4) {
+//     console.log("Hello",name1,name2,name3,name4);
+// }
+// names("Ali", "Anab","Geedi" ,"Farhia")
+
+
+// function myname(name,age) {
+//     console.log("Hello", name, "you are",age ,"Years old");
+// }
+// myname("Faraxaan", 20)
+// myname("Abdi",40)
+// myname("Abdi bile",70)
+
+
+
+
+
+//class work
+// function biggest(num1,num2,num3) {
+//     if (num1 > num2 && num1 > num3) {
+//         largestNumber = num1;
+//       }
+      
+      
+//       else if (num2 > num1 && num2 > num3) {
+//         largestNumber = num2;
+//       }
+//       else if (num1==num2 && num2==num3) {
+//         console.log("Makala weyno");
+//       }
+     
+      
+   
+//       else {
+//         largestNumber = num3;
+//       }
+     
+//       console.log("The largest number is: " + largestNumber);
+      
+
+// }
+// biggest(2,2,2)
+
+
+// function maxnum(num1,num2,num3) {
+//     if (num1>num2 && num>num3) {
+//         console.log(num1);
+//     }
+//     else if (num2>num1 && num2>num3) {
+//         console.log(num2);
+//     }
+//     else if (num3>num1 && num3>num2) {
+//         console.log(num3);
+//     }
+//     else{
+//         console.log("There are All Equal");
+//     }
+// }
+// maxnum(3,3,15)
+//retun function
+// function sayhi() {
+//     let name="Ali"
+//     return name;
+    
+// }
+// console.log(sayhi());
+
+// function addition() {
+//     let num1=10;
+//     let num2=20
+//     let sum= num1+num2
+//     return sum
+// }
+// console.log(addition());
+
+
+
+
+
+
+
+// function multiply(num1,num2,num3) {
+//     let result= num1*num2*num3
+//     return result
+    
+// }
+// console.log(multiply(2,2,5));
+//fat arrow
+// const sayhi=() =>{
+//     console.log("Abdi");
+// }
+//  sayhi()
+
+
+
+
+//  const name_age= (name,age) =>{
+//     console.log("Hello",name,"You are ",age ,"years Old");
+//  }
+//  name_age("Ali",25)
+
+
+
+
+
+// let myfriends=["Ali","Juweriyo","farxaan",10,"ABdi"]
+// let num=0
+// while (num< myfriends.length) {
+//     console.log(myfriends[num]);
+//     num++
+// }
+
+
+
+
+// let books=["book1","book2","book3","book4"]
+
+// let num=0
+// while (num< books.length) {
+//     console.log(books[num]);
+//     num++
+// }
+// books.push("book5")
+// console.log(books);
+// books.pop()
+// console.log(books);
+
+
+// books.unshift("book 0")
+// console.log(books);
+// books.shift()
+// console.log(books);
+
+
+// let position= books.indexOf('book2')
+// let removeditem= books.splice(position,1)
+// console.log(books);
+
+// //objects
+
+// let students={
+//     name:"farxaan",
+//     age:30,
+//     class:"Ca205",
+//     university:{
+//         campus:"three",
+//         class:"Ca204",
+//         ID:"c120241"
+//     },
+//     hobbies:["Reading", "writting","watching"],
+//    age: function(){
+//     console.log("40");
+//    }
+
+// }
+// students.age();
+// console.log(students);
+// console.log(students.age);
+// console.log(students.university.class);
+
+// console.log(students.hobbies[0]);
+
+
+
+let fruits=["Apple","Mango","Banana","canab","moos"]
+// let iskudar = fruits.join(", ")//adds all elements
+// console.log(iskudar);
+// fruits.sort()// sorts array in alphapatical order
+// console.log(fruits);
+// fruits.reverse()//reverses elements in the array
+// console.log(fruits);
+//splice() method will cgange the content of original array by removing or aading the element
+ 
+
+
+// fruits.splice(1,1)//remove
+// fruits.splice(2,0,"Ayaan")//adds element
+// console.log(fruits);
+
+//  fruits.forEach(function(item) {
+//     console.log(item);
+//  })
+
+
+// //filter
+// let ages= [14,34,56,40,15]
+
+// let newages =ages.filter(function (result) {
+
+
+//     return result >15
+
+// })
+// console.log(newages);
+
+
+// let names= ["Abdi","Ayaan","Geedi","Gurey"]
+// let  newNames= names.filter(function (result) {
+//     return result =="Gurey"
+    
+// })
+// console.log(newNames);
+
+//map()
+// let ages= [14,34,56,40,15]
+// let newages = ages.map((age)=>{
+//     console.log(age);
+// })
+
+
+//    DOM 
+
+// const element = document.getElementById("h1")
+// const text = document.getElementsByTagName("p")[0]
+
+
+// console.log(text);
+// console.log(element);
+
+// const className = document.getElementsByClassName("btn") [0]
+// const universal1= document.querySelector("#h1")
+
+// console.log(universal1);
+// console.log(className);
+
+// const element = document.getElementById("h1")
+// element.innerHTML="It is Written By JavaScript ❤❣"
+// element.style.color= "purple"
+// element.style.background = "orange"
+
+
+
+// const className = document.querySelector(".btn")
+// className.style.background ="orange"
+// className.style.color="white"
+// className.style.fontSize ="20px";
+
+// className.addEventListener("click",function () {
+//      element.style.color="red"
+//      body.style.backgroundColor="yellow"
+
+//      // element.style.backgroundColor="blue"
+     
+// })
+// const body = document.querySelector(".body")
+
+// className.addEventListener("click",function() {
+//      className.style.backgroundColor="purple"
+//      body.style.backgroundColor="yellow"
+     
+// })
+const lightbox= document.querySelector(".light-box")
+const button = document.querySelector(".btn1");
+const button2 = document.querySelector(".btn2")
+
+
+
+
+button.addEventListener("click",function() {
+     lightbox.style.backgroundColor="yellow"
+     button.style.color="red"
+     button2.style.color="black"
+          
+     })
+     button2.addEventListener("click",function() {
+          lightbox.style.backgroundColor="transparent"
+          button2.style.color="red"
+          button.style.color="black"
+                
+          })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
